a5ad7e266dab8ddad2a1136a85f0688cf3a8bdc4
Phuong <phuongit1306@gmail.com>
2025-03-17 22:57:11 +0700

    Move&RunPlayer and Animaiton Idle&Run

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer flipPlayer;
+    private Animator animator;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        flipPlayer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        Vector2 playerInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb.linearVelocity = playerInput.normalized * moveSpeed;
+
+        if (playerInput.x < 0)
+        {
+            flipPlayer.flipX = true;
+        }
+        else if (playerInput.x > 0)
+        {
+            flipPlayer.flipX = false;
+        }
+        //Player Run
+        if(playerInput != Vector2.zero)
+        {
+            animator.SetBool("isRun", true);
+        }
+        else 
+        {
+            animator.SetBool("isRun", false);
+        }
+    }   
+}
