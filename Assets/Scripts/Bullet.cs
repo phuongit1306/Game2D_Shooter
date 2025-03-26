a05bec99266605e212bdc5c169c83457c5540f2f
Phuong <phuongit1306@gmail.com>
2025-03-26 21:37:12 +0700

    Enemy HP

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,6 +5,7 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float bulletSpeed = 30f;
     [SerializeField] private float timeDestroyBullet = 0.5f;
 
+    [SerializeField] private float dmg = 20f;
     void Start()
     {
         Destroy(gameObject, timeDestroyBullet);
@@ -27,7 +28,7 @@ public class Bullet : MonoBehaviour
             Enemy enemy = collision.GetComponent<Enemy>();
             if(enemy != null)
             {
-                enemy.TakeDmg();
+                enemy.TakeDmg(dmg);
             }
             Destroy(gameObject);
         }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,13 +1,23 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Enemy : MonoBehaviour
 {
 
     [SerializeField] protected float enemyMoveSpeed = 3f;
     protected Player player;
+
+    [SerializeField] protected float maxHp = 100f;
+    protected float currentHp;
+
+    [SerializeField] private Image hpBar;
     protected virtual void Start()
     {
         player = FindAnyObjectByType<Player>();
+        currentHp = maxHp;
+        UpdateHpBar();
     }
 
     protected virtual void Update()
@@ -30,13 +40,28 @@ public abstract class Enemy : MonoBehaviour
         }
     }
 
-    public virtual void TakeDmg()
+    public virtual void TakeDmg(float dmg)
     {
-        Die();
+        currentHp -= dmg;
+        currentHp = Math.Max(currentHp, 0);
+        UpdateHpBar();
+        if (currentHp <= 0)
+        {
+            Die();
+        }
     }
 
     public virtual void Die()
     {
         Destroy(gameObject);
     }
+
+    protected void UpdateHpBar()
+    {
+        if(hpBar != null)
+        {
+            hpBar.fillAmount = currentHp / maxHp;
+        }
+    }
+
 }
